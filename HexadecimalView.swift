39e120db4336da7d4c3b9074c77d9bcfb5a23cd1
Joshua Ilagan <Jilagan6194@stu.d214.org>
2023-09-18 10:48:17 -0500

    New View

---
+++ HexadecimalView.swift
@@ -0,0 +1,20 @@
+//
+//  HexadecimalView.swift
+//  Converter
+//
+//  Created by Joshua S. Ilagan on 9/18/23.
+//
+
+import SwiftUI
+
+struct HexadecimalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HexadecimalView_Previews: PreviewProvider {
+    static var previews: some View {
+        HexadecimalView()
+    }
+}
