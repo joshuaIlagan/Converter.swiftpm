e1ce790c1854628a6566091818bd5284ab5fb4db
Brock Wrede <bee15dub@gmail.com>
2023-09-18 10:48:30 -0500

    Added My View

---
+++ Decimal to Binary View.swift
@@ -0,0 +1,20 @@
+//
+//  Decimal to Binary View.swift
+//  Converter
+//
+//  Created by Brock R. Wrede on 9/18/23.
+//
+
+import SwiftUI
+
+struct Decimal_to_Binary_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Decimal_to_Binary_View_Previews: PreviewProvider {
+    static var previews: some View {
+        Decimal_to_Binary_View()
+    }
+}
